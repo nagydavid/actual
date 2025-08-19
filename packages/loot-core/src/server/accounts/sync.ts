9e031e571b06bad0e5bb4de08f549bf8a13f0b14
Erik <realtwister@gmail.com>
2025-08-19 12:37:43 +0200

    add per bank processing for transactions

--- packages/loot-core/src/server/accounts/sync.ts
+++ packages/loot-core/src/server/accounts/sync.ts
@@ -34,9 +34,9 @@ import {
   mappingsFromString,
 } from '../util/custom-sync-mapping';
 
+import { downloadEnableBankingTransactions } from './enablebanking';
 import { getStartingBalancePayee } from './payees';
 import { title } from './title';
-import { downloadEnableBankingTransactions } from './enablebanking';
 
 function BankSyncError(type: string, code: string, details?: object) {
   return { type: 'BankSyncError', category: type, code, details };
@@ -1007,7 +1007,11 @@ export async function syncAccount(
       newAccount,
     );
   } else if (acctRow.account_sync_source === 'enablebanking') {
-    download = await downloadEnableBankingTransactions(acctId, syncStartDate, bankId);
+    download = await downloadEnableBankingTransactions(
+      acctId,
+      syncStartDate,
+      bankId,
+    );
   } else {
     throw new Error(
       `Unrecognized bank-sync provider: ${acctRow.account_sync_source}`,
